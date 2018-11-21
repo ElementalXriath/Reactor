ee1f808c6a9fc57fc19215fa20d6d7db81dac160
Joseph Thompson <joe.thompson838@gmail.com>
2018-11-21 00:41:55 -0500

    added dummy data to server, have a couple users, and components

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import MainHeader from './Components/header.js';
 import MainView from './Views/main_view';
-import ComponentsView from './Views/ComponentsView';
-import ViewsView from './Views/ViewsView';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
@@ -12,8 +10,6 @@ class App extends Component {
       <div className="App">
           <MainHeader/>
           <Route path='/' exact component={MainView} />
-          <Route path='/components'exact component={ComponentsView} />
-          <Route path='/views' exact component={ViewsView} />
       </div>
 
     );
