c596830b921a241e928990fec29970d41bccc272
Asleepless Knight <41958127+ElementalXriath@users.noreply.github.com>
2018-11-27 18:25:25 -0500

    turned all single files into folders and files to jx

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 // you can also import directly from a folder and it looks for index.js first
-import MainHeader from './Components/Header';
+import MainHeader from './Components/Header/index';
 // you do not need .js on the end, it will only look for a js file
 // import MainHeader from './Components/header.js';
-import MainView from './Views/main_view';
-import ViewsView from './Views/ViewsView';
-import ComponentsView from './Views/ComponentsView';
+import MainView from './Views/mainlanding';
+import ViewsView from './Views/ViewsView/index';
+import ComponentsView from './Views/ComponentsView/index';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
