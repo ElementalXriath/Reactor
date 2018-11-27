44f89e0cf72628a281c9bdbdd9a4b5ac162b86ed
Asleepless Knight <41958127+ElementalXriath@users.noreply.github.com>
2018-11-26 21:19:05 -0500

    Component Entry Form

---
+++ UI/src/Components/CompForm/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class ComponentForm extends Component {
+  render() {
+    return (
+
+      <div className="ComponentForm">
+    
+
+          
+      </div>
+
+    );
+  }
+}
+
+export default ComponentForm;
