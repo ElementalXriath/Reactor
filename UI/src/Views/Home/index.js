5b3a44cc195cf797757d8c2b2e616af6b661eacd
Asleepless Knight <41958127+ElementalXriath@users.noreply.github.com>
2018-11-29 21:23:28 -0500

    Update index.js

--- UI/src/Views/Home/index.js
+++ UI/src/Views/Home/index.js
@@ -1,112 +1,63 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
-    CardSubtitle, CardBody, Alert } from 'reactstrap';
+    CardSubtitle, CardBody } from 'reactstrap';
 import Logo from './logo.jpg';
-import { FaExpand, FaUikit, FaAtom, FaGem, FaNpm, FaMagento, FaUsers, FaFulcrum } from "react-icons/fa";
+import { FaExpand, FaUikit, FaAtom, FaGem  } from "react-icons/fa";
 
 export default class MainView extends React.Component {
   render() {
     return (
-<div>
-      <Alert color="info" outline p="1" className="text-center">
-      <h4>R[3]ACTOR</h4>
-    </Alert>
+
+
     <Container className="text-center">
         <br />
         <Row> 
         <Col xs="12" sm="12" className="text-center">
-     
-            
-            
-           <img src={Logo} className="img-rounded m-0" /> <br /><br />
-          
+            <h1>Reactor</h1>
+           <img src={Logo} className="img-thumbnail" /> 
+             
         </Col>
         </Row>
         <br />
         <Row>       
-        <Col xs="6" sm="3" lg="3">
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary " >
-        <CardTitle><h3><FaUikit /></h3></CardTitle>
+        <Col xs="6" sm="6">
+        <Card body className="shadow-lg p-3 mb-5 bg-white rounded" >
+        <CardTitle><FaUikit /></CardTitle>
           <CardTitle>Components</CardTitle>
-          <CardText>Component Lib.</CardText>
-          <Button className="shadow rounded" color="primary" >Go</Button>
+          <CardText>Search Reactors Component Lib.</CardText>
+          <Button color="primary">Go</Button>
         </Card>
       </Col>
-      <Col xs="6" sm="3" lg="3" >
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary">      
-        <CardTitle><h3><FaExpand /></h3></CardTitle>
+      <Col xs="6" sm="6">
+        <Card body className="shadow-lg p-3 mb-5 bg-white rounded">      
+        <CardTitle><FaExpand /></CardTitle>
           <CardTitle>Views</CardTitle>
-          <CardText>Views Lib.</CardText>
-          <Button className="shadow rounded" color="success">Go</Button>
-        </Card>
-      </Col>
-            
-        <Col xs="6" sm="3" lg="3">
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary">
-        <CardTitle><h3><FaAtom /></h3></CardTitle>
-          <CardTitle>Widgets</CardTitle>
-          <CardText>Widget Lib.</CardText>
-          <Button className="shadow rounded" color="danger">Go</Button>
-        </Card>
-      </Col>
-      <Col xs="6" sm="3" lg="3">
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary">    
-        <CardTitle><h3><FaGem /></h3></CardTitle>
-          <CardTitle>Exclusive</CardTitle>
-          <CardText>X Lib.</CardText>
-          <Button className="shadow rounded" color="warning">Go</Button>
+          <CardText>Serach Reactors Views Lib.</CardText>
+          <Button>Go</Button>
         </Card>
       </Col>
         </Row>
-
-
-
-
-
-
-        <br />
-
-
-
-
         <Row>       
-        <Col xs="6" sm="3" lg="3">
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary" >
-        <CardTitle><h3><FaNpm /></h3></CardTitle>
-          <CardTitle>NPMS</CardTitle>
-          <CardText>Depedancies</CardText>
-          <Button className="shadow rounded" color="dark" outline >Go</Button>
+        <Col xs="6" sm="6">
+        <Card body className="shadow-lg p-3 mb-5 bg-white rounded">
+        <CardTitle><FaAtom /></CardTitle>
+          <CardTitle>Widgets</CardTitle>
+          <CardText>Search Reactors Widget Lib.</CardText>
+          <Button color="primary">Go</Button>
         </Card>
       </Col>
-      <Col xs="6" sm="3" lg="3" >
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary">      
-        <CardTitle><h3><FaMagento /></h3></CardTitle>
-          <CardTitle>Cool Stuff</CardTitle>
-          <CardText>Reactor Feed</CardText>
-          <Button className="shadow rounded" color="dark" outline>Go</Button>
-        </Card>
-      </Col>          
-        <Col xs="6" sm="3" lg="3">
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary">
-        <CardTitle><h3><FaUsers /></h3></CardTitle>
-          <CardTitle>Forums</CardTitle>
-          <CardText>Forums Lib.</CardText>
-          <Button className="shadow rounded" color="dark" outline>Go</Button>
-        </Card>
-      </Col>
-      <Col xs="6" sm="3" lg="3">
-        <Card body className="shadow-lg p-3 mb-5 bg-white rounded border-secondary">    
-        <CardTitle><h3><FaFulcrum/></h3></CardTitle>
-          <CardTitle>Hall Of Fame</CardTitle>
-          <CardText>HOF Lib.</CardText>
-          <Button className="shadow rounded" color="dark" outline>Go</Button>
+      <Col xs="6" sm="6">
+        <Card body className="shadow-lg p-3 mb-5 bg-white rounded">    
+        <CardTitle><FaGem /></CardTitle>
+          <CardTitle>Exclusive</CardTitle>
+          <CardText>Serach Reactors X Lib.</CardText>
+          <Button>Go</Button>
         </Card>
       </Col>
         </Row>
-        <Button className="shadow rounded" color="info">Whats Reactor ???</Button>
       </Container>
-     </div>
+     
 
         );
     }
